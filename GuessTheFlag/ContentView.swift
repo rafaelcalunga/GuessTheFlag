ec77930686eb1ef6abfb37e3072e30d0691dfe10
Rafael Calunga <rafaelcalunga@gmail.com>
2020-08-12 23:40:16 -0400

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Rafael Calunga on 2020-08-12.
+//  Copyright © 2020 Rafael Calunga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
