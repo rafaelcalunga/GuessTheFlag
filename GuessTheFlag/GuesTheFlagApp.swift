2d28ff84b0debaab21e0f4bc8eac5eccd4a5e1cc
Rafael Calunga <rafaelcalunga@gmail.com>
2020-09-24 19:03:01 -0400

    Simplifying the AppDelegate thanks to iOS 14

---
+++ GuessTheFlag/GuesTheFlagApp.swift
@@ -0,0 +1,18 @@
+//
+//  GuesTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Rafael Calunga on 2020-08-12.
+//  Copyright © 2020 Rafael Calunga. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GuesTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
