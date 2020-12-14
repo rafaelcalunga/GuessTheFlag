ee925d5a5dde7359b6f4342c3c655d750af447fb
Rafael Calunga <rafaelcalunga@gmail.com>
2020-12-14 11:11:50 -0500

    Fixing accessibility issues

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct FlagImage: View {
     var name: String
+    var label: [String: String]
     
     var body: some View {
         Image(name)
@@ -17,6 +18,7 @@ struct FlagImage: View {
             .clipShape(Capsule())
             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
             .shadow(color: .black, radius: 2)
+            .accessibility(label: Text(label[name, default: "Unknown flag"]))
     }
 }
 
@@ -30,6 +32,20 @@ struct ContentView: View {
     @State private var isCorrectAnswer = false
     
     @State private var animationAmount = 0.0
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     
     var body: some View {
         
@@ -60,14 +76,14 @@ struct ContentView: View {
                     }) {
                         if self.showScore {
                             if number == self.correctAnswer {
-                                FlagImage(name: self.countries[number])
+                                FlagImage(name: self.countries[number], label: self.labels)
                                     .rotation3DEffect(.degrees(self.animationAmount), axis: (x: 0, y: 1, z: 0))
                             } else {
-                                FlagImage(name: self.countries[number])
+                                FlagImage(name: self.countries[number], label: self.labels)
                                     .opacity(0.25)
                             }
                         } else {
-                            FlagImage(name: self.countries[number])
+                            FlagImage(name: self.countries[number], label: self.labels)
                         }
                     }
                 }
